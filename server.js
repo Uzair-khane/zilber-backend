ed01fba7fdf95438d26e0729fac005aadebf2bbe
Uzair-khane <uzairkhanar123@gmail.com>
2026-04-13 12:13:31 +0500

    fix: cors update

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path'); // 1. Ye add karein (Built-in hota hai)
+const path = require('path');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
+
 const app = express();
-app.use(cors());
+
+// ✅ CORS fix
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Backend Server Running Successfully!');
 });
-// 2. Ye line images ko browser mein dikhane ke liye ZAROORI hai
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
-
 app.use('/api/orders', orderRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server start on port ${PORT} `);
+  console.log(`Server start on port ${PORT}`);
 });
